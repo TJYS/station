1ff9e0451902777e3dce2aabd10014fecd78770a
TJYS <w199404081@gmail.com>
2018-12-22 19:21:40 +0800

    修改包路径

--- station-common/src/main/java/com/huace/station/common/vo/Response.java
+++
@@ -1,5 +0,0 @@
-package com.huace.station.common.vo;
-
-public class Response<T> {
-    private int status = 0;
-}
--- station-server/src/main/java/com/huace/station/server/repository/StationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.huace.station.server.repository;
-
-import com.huace.station.common.domain.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, Integer> {
-     boolean existsByIdIsNotAndNameIs(Integer id, String name);
-}
