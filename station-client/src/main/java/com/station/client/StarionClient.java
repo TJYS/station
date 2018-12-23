a1bcc07164764db79cc728cfea47b8305283107d
TJYS <w199404081@gmail.com>
2018-12-23 19:01:21 +0800

    feign添加Hystrix支持

--- station-client/src/main/java/com/station/client/StarionClient.java
+++ station-client/src/main/java/com/station/client/StarionClient.java
@@ -1,11 +1,33 @@
 package com.station.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "station-server")
+/**
+ * 其他项目在引用此模块时记得添加如下配置
+ * @<code>
+ *     feign:
+ *       hystrix:
+ *         enabled: true
+ * </code>
+ * 且主类要配置包扫描,如下
+ * {@code
+ *  @EnableFeignClients(basePackages = "com.station.client")
+ *  @ComponentScans(@ComponentScan("com.station.client"))
+ * }
+ */
+@FeignClient(name = "station-server", fallback = StarionClient.StarionClientFallback.class)
 public interface StarionClient {
     @GetMapping("/station/start/{id:\\d+}")
     String start(@PathVariable Integer id);
+
+    @Component
+    public static class StarionClientFallback implements StarionClient{
+        @Override
+        public String start(Integer id) {
+            return null;
+        }
+    }
 }
